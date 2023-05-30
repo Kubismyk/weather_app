b3337e60e58e1454e3bb391f3250ba50428e192c
levaniii <mr.charuashvili@gmail.com>
2023-05-31 01:48:04 +0400

    top tab bar

--- weather_app/ViewController.swift
+++ weather_app/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import Foundation
+import Tabman
+import Pageboy
 
-class ViewController: UIViewController {
+class ViewController:TabmanViewController{
+    
 
+    private var viewControllers = [UIViewController(),ViewControllerSecond(),ViewControllerThird()]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.dataSource = self
+
+        // Create bar
+        let bar = TMBar.ButtonBar()
+        bar.layout.transitionStyle = .snap // Customize
+        bar.backgroundColor = .none
+        addBar(bar, dataSource: self, at: .top)
+        let viewWidth: CGFloat = self.view.bounds.width
+        let viewHeight: CGFloat = self.view.bounds.height
+        let vcCount = viewControllers.count + 1
+        let test = round(Double(viewControllers.count)) + 5
+        bar.layout.contentInset = UIEdgeInsets(top: 0, left: viewWidth/CGFloat(vcCount) - test, bottom: 0, right: 0)
     }
-
-
 }
 
+
+
+extension ViewController: PageboyViewControllerDataSource, TMBarDataSource {
+    func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
+        let title = "Page \(index)"
+        return TMBarItem(title: title)
+    }
+    
+    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
+        return viewControllers.count
+    }
+    
+    func viewController(for pageboyViewController: PageboyViewController, at index: PageboyViewController.PageIndex) -> UIViewController? {
+        return viewControllers[index]
+    }
+    
+    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
+        nil
+    }
+}
---
+++ weather_app/ViewControllerSecond.swift
@@ -0,0 +1,28 @@
+//
+//  ViewControllerSecond.swift
+//  weather_app
+//
+//  Created by Levan Charuashvili on 30.05.23.
+//
+
+import UIKit
+
+class ViewControllerSecond: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
